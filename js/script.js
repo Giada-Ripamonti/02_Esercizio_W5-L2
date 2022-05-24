cfed8bfd1cd0245213b2c238d61c710a12ab452b
Giada-Ripamonti <ripamontigiada@gmail.com>
2022-05-24 19:33:04 +0200

    second-commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 let budget = prompt('Qual è il tuo budget?')
 console.log(budget);
 
-document.getElementById('budget').innerHTML = budget;
+document.getElementById('budgetStart').innerHTML = '£ ' + +(budget);
 
 function showItems() {
     document.getElementById('container').style.visibility = "visible";
+    document.getElementById('showItems').style.visibility = "hidden";
 }
 
 let imgs = ['plant1', 'plant2', 'plant3']
@@ -12,11 +13,32 @@ let imgs = ['plant1', 'plant2', 'plant3']
 let card = document.getElementById('container');
 
 imgs.forEach(img => {
-    card.innerHTML = '<div class="card">' +
-                     '<img src="assets/imgs/'++img'.png">' +
-                     '<p>Plant Name</p>' +
-                     '<p>Price: <span id="price1"></span></p>' +
-                     '<button class="buyBtn" onclick="buyItem()">buy</button>' +
+    let productPrice = fixPrice();
+    card.innerHTML += '<div class="card">' +
+                     '<img src="assets/imgs/'+img+'.png">' +
+                     '<h4>Plant Name: '+img+'</h4>' +
+                     '<p>£ '+productPrice+'</p>' +
+                     '<button class="buyBtn" onclick="buyItem('+productPrice+')">buy</button>' +
                      '</div>'
 })
 
+function fixPrice() {
+    let price = Math.random() * 80;
+    return price.toFixed(2);
+}
+
+function addBudget() {
+    let addValue = document.getElementById('budgetValue').value;
+    budgetStart.innerHTML = parseInt(budget) + parseInt(addValue);
+}
+
+function buyItem(productPrice) {
+    if(budget > productPrice) {
+        budget = +(budget) - productPrice;
+        budgetStart.innerHTML = parseInt(budget).toFixed(2);
+    } else if(budget < productPrice){
+        alert('Out of budget! =(');
+    } else(budget < (budget/2));{   
+        alert('Sei a metà del tuo budget!');
+    }       
+}
